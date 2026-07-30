b8821c1177affe4667963fa4a3d44288ec0a6f80
Hidehito Asano <h.asano0612@gmail.com>
2026-07-30 15:25:31 +0900

    chore(site): sync Phase 1 typography output (#1662)

---
+++ docs/_next/static/chunks/pages/_app-89cdc94cb83803a8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{6230:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(7429)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(6230),n(7225))),_N_E=_.O()}]);
